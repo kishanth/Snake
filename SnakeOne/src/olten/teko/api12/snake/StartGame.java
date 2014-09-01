29ad05729870154db30c9964ccd1867eb7ef4201
geocast90 <Stefan@192.168.11.46>
2014-09-01 17:14:53 +0200

    Name Abfragen und Testen ob Namen vorhanden ist

--- SnakeOne/src/olten/teko/api12/snake/StartGame.java
+++ SnakeOne/src/olten/teko/api12/snake/StartGame.java
@@ -3,78 +3,38 @@ package olten.teko.api12.snake;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+
 
 public class StartGame extends JFrame{
     
+    String name;
+    
     Menue menu = new Menue();
+
     
     public StartGame() {
         
+        
+        if(!testName(name)){
+            name = (String)JOptionPane.showInputDialog(rootPane, "Namen Eingeben", "");            
+        }
+        
         setMenuBar(menu.getMenuBar());
-        add(new Init());
+        add(new Init(name));
         setResizable(false);        
         pack();
-        
+
         setTitle("Snake");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+                
     }
     
-    private class Menue extends Frame implements ActionListener {
-        
-        //Dialog dia = new Dialog();
-        
-        MenuBar mbar;
-        Menu menu,submenu;
-        MenuItem m1;
-        
-        public Menue(){
-            
-            mbar = new MenuBar();
-            menu = new Menu("Datei");
-            m1 = new MenuItem("Neues Spiel");
-            
-            
-            menu.add(m1);
-            mbar.add(menu);
-            setMenuBar(mbar);
-            m1.addActionListener(this);
-           
-            
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if(e.getSource() == m1){
-                Dialog dia = new Dialog();
-            }
-            
-            
-        }
-    }
-    
-    private class Dialog extends JFrame {
-        
-        JDialog myDialog = new JDialog();
-        JTextField tf = new JTextField();
-        
-        public Dialog() {
-            myDialog.setTitle("New Player");
-            myDialog.setSize(250, 100);
-            myDialog.setModal(true);
-            myDialog.setVisible(true);
-            myDialog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            myDialog.add(tf);
-            
-        }
-        
-        
+    private boolean testName(String name){
+        return name != null;
         
     }
+ 
 }
-
-
