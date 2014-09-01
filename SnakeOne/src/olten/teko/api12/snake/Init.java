15f17f35180c69f40c553621c44870258ac27629
geocast90 <Stefan@stefans-mbp.fritz.box>
2014-09-01 10:33:22 +0200

    Klasse zum Initialisieren

---
+++ SnakeOne/src/olten/teko/api12/snake/Init.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package olten.teko.api12.snake;
+
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author Stefan
+ */
+public class Init extends Logic {
+    
+    // Konstruktor zum Aufrufen vom Spiel
+    Init(){
+        initGame();
+    }
+    
+    // Methode Ausführung überschreiben
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (inGame) {
+            
+            checkPi(apple);
+            checkPi(pill);
+            checkPi(fungus);
+            checkPi(banana);
+
+            checkCollision();
+            move();
+        }
+
+        repaint();
+    }
+        
+
+    private void initGame() {
+
+        dots = 20;
+
+        for (int z = 0; z < dots; z++) {
+            x[z] = 50 - z * 10;
+            y[z] = 50;
+        }
+
+        locatePi(apple);
+        locatePi(pill);
+        locatePi(fungus);
+        locatePi(banana);
+
+        timer = new Timer(DELAY, this);
+        
+    }
+}
