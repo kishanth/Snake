ec322fc1c297d7119b9fcff4813a88d8cdba546f
geocast90 <Stefan@stefans-mbp.fritz.box>
2014-09-01 11:53:33 +0200

    Update

--- SnakeOne/src/olten/teko/api12/snake/Snake.java
+++ SnakeOne/src/olten/teko/api12/snake/Snake.java
@@ -5,33 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 
-public class Snake extends JFrame {
-
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-
-	public Snake() {
-
-        add(new Logic());
-        
-        setResizable(false);
-        pack();
-        
-        setTitle("Snake");
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    
+public class Snake {
 
     public static void main(String[] args) {
         
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {                
-                JFrame snakeFrame = new Snake();
+                JFrame snakeFrame = new StartGame();
                 snakeFrame.setVisible(true);                
             }
         });
