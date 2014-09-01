0be4f664a703c416d97bc335f53ee8445ac220ae
geocast90 <Stefan@stefans-mbp.fritz.box>
2014-09-01 15:43:34 +0200

    Anfang von Startdialog für Namen

--- SnakeOne/src/olten/teko/api12/snake/StartGame.java
+++ SnakeOne/src/olten/teko/api12/snake/StartGame.java
@@ -1,18 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package olten.teko.api12.snake;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
-/**
- *
- * @author Stefan
- */
 public class StartGame extends JFrame{
     
     Menue menu = new Menue();
@@ -30,7 +24,9 @@ public class StartGame extends JFrame{
         
     }
     
-    private class Menue extends Frame {
+    private class Menue extends Frame implements ActionListener {
+        
+        //Dialog dia = new Dialog();
         
         MenuBar mbar;
         Menu menu,submenu;
@@ -46,8 +42,38 @@ public class StartGame extends JFrame{
             menu.add(m1);
             mbar.add(menu);
             setMenuBar(mbar);
+            m1.addActionListener(this);
+           
             
         }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == m1){
+                Dialog dia = new Dialog();
+            }
+            
+            
+        }
+    }
+    
+    private class Dialog extends JFrame {
+        
+        JDialog myDialog = new JDialog();
+        JTextField tf = new JTextField();
+        
+        public Dialog() {
+            myDialog.setTitle("New Player");
+            myDialog.setSize(250, 100);
+            myDialog.setModal(true);
+            myDialog.setVisible(true);
+            myDialog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            myDialog.add(tf);
+            
+        }
+        
+        
+        
     }
 }
 
