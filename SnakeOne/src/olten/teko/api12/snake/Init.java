9e6a04151b026c15fafc753c663ba14b8c41cfa0
geocast90 <Stefan@dyn-125-149.o.teko.ch>
2014-09-03 16:02:07 +0200

    Können nun soviele Objekte wie gewollt erzeugt werden

--- SnakeOne/src/olten/teko/api12/snake/Init.java
+++ SnakeOne/src/olten/teko/api12/snake/Init.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package olten.teko.api12.snake;
 
@@ -27,32 +23,43 @@ public class Init extends Logic {
 
         if (inGame) {
             
-            checkPi(apple);
-            checkPi(pill);
-            checkPi(fungus);
-            checkPi(banana);
+            movePill();
+            
+            for (PowerItems item : coll.items) {
+                checkPi(item);          
+            }
 
             checkCollision();
             move();
         }
 
         repaint();
-    }
-        
-
+    }        
+    
     private void initGame() {
         
         dots = 3;
+        
+    // 1. Name von Bilddatei ohne erweiterung  2. zu/abnahme von Punkten 3. zu/abnahme dots von Schlange 4. zu/abnahme von Zeit (-) schneller (+) langsamer
+        coll.addApple("apple", 10, 1, -1);
+        coll.addApple("apple", 10, 1, -1);
+        coll.addApple("apple", 10, 1, -1);
+        coll.addBanana("banana", 50, 0, 0);
+        coll.addFungus("fungus", -10, -2, -2);
+        coll.addFungus("fungus", -10, -2, -2);
+        coll.addFungus("fungus", -10, -2, -2);
+        coll.addFungus("fungus", -10, -2, -2);
+        coll.addPill("pill", 20, 2, 2);
+        
 
         for (int z = 0; z < dots; z++) {
             x[z] = 50 - z * 10;
             y[z] = 50;
         }
 
-        locatePi(apple);
-        locatePi(pill);
-        locatePi(fungus);
-        locatePi(banana);
+        for (PowerItems item : coll.items) {
+            locatePi(item);          
+        }
 
         timer = new Timer(DELAY, this);
         
